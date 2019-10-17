1d65b37a38da484c086018306905e3e4cb0a03bd
Martin Heidegger <martin.heidegger@gmail.com>
2019-10-17 14:54:33 +0900

    Added exemplary color setup

---
+++ src/colors.ts
@@ -0,0 +1,12 @@
+export enum Colors {
+  consentoOrange = '#da552f',
+  invalid = '#00ac7d',
+  highlightText = '#007aff',
+  consentoBlue = '#69bac9',
+  consentoGrey = '#484848',
+  lightGrey = '#aca9a9',
+  dirty = '#f3f3f3',
+  bright = '#fcfcfc',
+  white = '#ffffff',
+  mainGray = '#454545'
+}

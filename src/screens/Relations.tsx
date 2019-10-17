580004eda3cbca8b35d3d0d8ecae9153458e601d
Martin Heidegger <martin.heidegger@gmail.com>
2019-10-17 15:01:06 +0900

    Added icon example

--- src/screens/Relations.tsx
+++ src/screens/Relations.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { View, Text } from 'react-native'
+import { Fonts } from '../fonts'
 
 const mapStateToProps = state => state
 const mapDispatchToProps = dispatch => ({})
 
 function Open ({}) {
-  return <View style={{ justifyContent: 'center', flex: 1 }}>
-    <Text>{"Relations"}</Text>
+  return <View style={{ justifyContent: 'center', flex: 1  }}>
+    <Text style={{fontFamily: Fonts.PalanquinDarkBold, fontSize: 72, color: '#000'}}>{"Relations"}</Text>
   </View>
 }
 

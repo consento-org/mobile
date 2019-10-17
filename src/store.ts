0ebf2969ef365098a002bc8dc0ad36e5c658df88
Martin Heidegger <martin.heidegger@gmail.com>
2019-10-17 15:05:14 +0900

    Fixed reference to old module

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { reduce } from './reducers'
 import { consentoMiddleware } from './middleware/consento'
-import { backButtonMiddleware } from './middleware/navigation'
 
-export const store = createStore(reduce, applyMiddleware(thunk, consentoMiddleware, backButtonMiddleware))
+export const store = createStore(reduce, applyMiddleware(thunk, consentoMiddleware))
 export default store

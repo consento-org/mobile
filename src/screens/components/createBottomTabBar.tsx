22f59e6073b183ce92a6c192b08c629476f813e2
Martin Heidegger <martin.heidegger@gmail.com>
2019-11-14 15:36:48 +0900

    Extracted the creation of the bottom tab bar into own module

---
+++ src/screens/components/createBottomTabBar.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from 'react'
+import { Text } from 'react-native'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { elementBottomNav } from '../../styles/component/elementBottomNav'
+import { elementBottomNavConsentosActive } from '../../styles/component/elementBottomNavConsentosActive'
+import { elementBottomNavConsentosResting } from '../../styles/component/elementBottomNavConsentosResting'
+import { resources } from '../../resources'
+
+// https://reactnavigation.org/docs/en/bottom-tab-navigator.html#bottomtabnavigatorconfig
+const config = {
+  tabBarOptions: {
+    showLabel: true,
+    activeBackgroundColor: elementBottomNav.backgroundColor,
+    inactiveBackgroundColor: elementBottomNav.backgroundColor,
+    style: {
+      borderTopColor: elementBottomNav.borderTop.border.fill.color,
+      borderTopWidth: elementBottomNav.borderTop.border.thickness,
+      height: elementBottomNav.height
+    },
+    tabStyle: {
+      padding: elementBottomNav.height - elementBottomNav.consento.place.bottom
+    }
+  }
+}
+
+const { t, icon } = resources.ctx('navigation')
+
+function createTabs (tabs: { [key: string]: () => ReactNode }) {
+  const res = {}
+  for (const key in tabs) {
+    res[key] = {
+      path: key,
+      screen: tabs[key],
+      navigationOptions: {
+        tabBarLabel: ({ focused }) => <Text style={ focused ? elementBottomNavConsentosActive.title.style : elementBottomNavConsentosResting.title.style }>
+          { t(key) }
+        </Text>,
+        tabBarIcon: opts => icon(key, opts),
+      }
+    }
+  }
+  return res
+}
+
+export function createBottomTabBar (tabs: { [key: string]: () => ReactNode}) {
+  return createBottomTabNavigator(createTabs(tabs), config)
+}
+
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,67 +1,24 @@
 import React from 'react'
 import { createAppContainer, NavigationContainerProps, NavigationContainerComponent } from 'react-navigation'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabBar } from './components/createBottomTabBar'
 import { NotificationTestScreen } from './NotificationTest'
-import { ReactNode } from 'react'
 import { VaultsScreen } from './Vaults'
 import { RelationsScreen } from './Relations'
 import { ConsentosScreen } from './Consentos'
-import { resources } from '../resources'
-import { Text, StyleSheet } from 'react-native'
-import { elementBottomNavConsentosResting } from '../styles/component/elementBottomNavConsentosResting'
-import { elementBottomNavConsentosActive } from '../styles/component/elementBottomNavConsentosActive'
-import { BottomTabBarOptions } from 'react-navigation-tabs/src/types'
-import { elementBottomNav } from '../styles/component/elementBottomNav'
+import { Text } from 'react-native'
 import { Vault } from './Vault'
-
-// https://reactnavigation.org/docs/en/bottom-tab-navigator.html#bottomtabnavigatorconfig
-const tabBarOptions: BottomTabBarOptions = {
-  showLabel: true,
-  activeBackgroundColor: elementBottomNav.backgroundColor,
-  inactiveBackgroundColor: elementBottomNav.backgroundColor,
-  ... StyleSheet.create({
-    style: {
-      borderTopColor: elementBottomNav.borderTop.border.fill.color,
-      borderTopWidth: elementBottomNav.borderTop.border.thickness,
-      height: elementBottomNav.height
-    },
-    tabStyle: {
-      padding: elementBottomNav.height - elementBottomNav.consento.place.bottom
-    }
-  })
-}
 
 function init () {
   try {
-    const { t, icon } = resources.ctx('navigation')
-    const tabs = (tabs: { [key: string]: ReactNode }) => {
-      const res = {}
-      for (const key in tabs) {
-        // console.log(icon(key))
-        res[key] = {
-          path: key,
-          screen: tabs[key],
-          navigationOptions: {
-            tabBarLabel: ({ focused }) => {
-              return <Text style={ focused ? elementBottomNavConsentosActive.title.style : elementBottomNavConsentosResting.title.style }>{ t(key) }</Text>
-            },
-            tabBarIcon: opts => icon(key, opts),
-          }
-        }
-      }
-      return res
-    }
     const AppNavigator = createStackNavigator({
       main: {
         path: '',
-        screen: createBottomTabNavigator(tabs({
-          vaults: VaultsScreen,
-          consentos: ConsentosScreen,
-          relations: RelationsScreen,
-          notificationTest: NotificationTestScreen
-        }), {
-          tabBarOptions
+        screen: createBottomTabBar({
+          vaults: () => <VaultsScreen />,
+          consentos: () => <ConsentosScreen />,
+          relations: () => <RelationsScreen />,
+          notificationTest: () => <NotificationTestScreen />
         })
       },
       vault: {

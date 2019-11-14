4e4ea82ad4ac72fec54d3d15e2082ec2e7036128
Martin Heidegger <martin.heidegger@gmail.com>
2019-11-14 15:43:10 +0900

    Updated design docs and output

--- src/styles/component/screen01Welcome.ts
+++ src/styles/component/screen01Welcome.ts
@@ -1,12 +1,16 @@
 // This file has been generated with expo-export, a Sketch plugin.
 import { Component, Polygon, ImagePlacement } from '../Component'
-import { Asset } from '../../Asset'
 import { Color } from '../Color'
+import { Asset } from '../../Asset'
 
 export class screen01WelcomeClass extends Component {
+  Oval = new Polygon({ x: 33.49, y: 9.65, w: 4, h: 4 }, '#e23e06ff', 0, null, [])
+  elementLogo = new Polygon({ x: 0, y: 0, w: 104, h: 104 }, Color.white, 0, null, [])
   c = new Polygon({ x: 0, y: 0, w: 35, h: 41.65 }, '#e23e06ff', 0, null, [])
   Shape = new Polygon({ x: 0, y: 0, w: 3.84, h: 19.07 }, '#e23e06ff', 0, null, [])
   Path = new Polygon({ x: 173.19, y: 48.67, w: 6.26, h: 7.81 }, null, 0, null, [])
+  OvalCopy = new Polygon({ x: 35.49, y: 14.65, w: 4, h: 4 }, '#e23e06ff', 0, null, [])
+  OvalCopy2 = new Polygon({ x: 36.49, y: 20.65, w: 4, h: 4 }, '#e23e06ff', 0, null, [])
   elementWelcome = new ImagePlacement(Asset.elementWelcome, { x: 77, y: 274.5, w: 217, h: 224.5 })
   constructor () {
     super('screen01Welcome', 375, 812, Color.coral)

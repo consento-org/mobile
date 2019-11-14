7ecbd7e278ef3f365f224fcb375a6251247a7e97
Martin Heidegger <martin.heidegger@gmail.com>
2019-11-14 15:40:04 +0900

    Adding empty screen display

--- src/screens/Consentos.tsx
+++ src/screens/Consentos.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { styles } from '../styles'
 import { EmptyView } from './components/EmptyView'
 import { TopNavigation } from './components/TopNavigation'
-import { Asset } from '../Asset'
-import { Slice9Button } from '../Slice9Button'
-import { elementSealVaultActive } from '../styles/component/elementSealVaultActive'
 import { elementConsentosEmpty } from '../styles/component/elementConsentosEmpty'
 
 const mapStateToProps = state => state
--- src/screens/Relations.tsx
+++ src/screens/Relations.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { View, Text } from 'react-native'
-import { Fonts } from '../fonts'
 import { styles } from '../styles'
 import { TopNavigation } from './components/TopNavigation'
+import { EmptyView } from './components/EmptyView'
 import { elementRelationsEmpty } from '../styles/component/elementRelationsEmpty'
 
 const mapStateToProps = state => state
 const mapDispatchToProps = dispatch => ({})
 
 function Open ({}) {
-  return <View style={ styles.screen }>
+  return <View style={{ ...styles.screen }}>
     <TopNavigation title="Relations"/>
     <EmptyView prototype={ elementRelationsEmpty } />
   </View>

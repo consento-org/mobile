c4eb682eda6b3282d06bbb602972751f3d338187
Martin Heidegger <martin.heidegger@gmail.com>
2019-12-06 02:22:26 +0900

    amend

--- src/styles/component/screen01Welcome.ts
+++ src/styles/component/screen01Welcome.ts
@@ -1,21 +1,14 @@
 // This file has been generated with expo-export, a Sketch plugin.
-import { Component, Polygon, ImagePlacement } from '../Component'
-import { Color } from '../Color'
+import { Component, ImagePlacement } from '../Component'
 import { Asset } from '../../Asset'
+import { Color } from '../Color'
 
 /* eslint-disable lines-between-class-members */
 export class Screen01WelcomeClass extends Component {
-  Oval = new Polygon({ x: 33.49, y: 9.65, w: 4, h: 4 }, '#e23e06ff', 0, null, [])
-  elementLogo = new Polygon({ x: 0, y: 0, w: 104, h: 104 }, Color.white, 0, null, [])
-  c = new Polygon({ x: 0, y: 0, w: 35, h: 41.65 }, '#e23e06ff', 0, null, [])
-  Shape = new Polygon({ x: 0, y: 0, w: 3.84, h: 19.07 }, '#e23e06ff', 0, null, [])
-  Path = new Polygon({ x: 173.19, y: 48.67, w: 6.26, h: 7.81 }, null, 0, null, [])
-  OvalCopy = new Polygon({ x: 35.49, y: 14.65, w: 4, h: 4 }, '#e23e06ff', 0, null, [])
-  OvalCopy2 = new Polygon({ x: 36.49, y: 20.65, w: 4, h: 4 }, '#e23e06ff', 0, null, [])
-  elementWelcome: ImagePlacement
+  illustration: ImagePlacement
   constructor () {
     super('screen01Welcome', 375, 812, Color.coral)
-    this.elementWelcome = new ImagePlacement(Asset.elementWelcome, { x: 77, y: 274.5, w: 217, h: 224.5 }, this)
+    this.illustration = new ImagePlacement(Asset.illustrationWelcome, { x: 78.85, y: 276, w: 216, h: 224 }, this)
   }
 }
 

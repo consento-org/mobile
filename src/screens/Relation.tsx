59b2c4300952cdd4b2e7743e22c56916eef8b1ad
Martin Heidegger <martin.heidegger@gmail.com>
2020-01-03 01:40:21 +0900

    Added delete function to Relation display

--- src/screens/Relation.tsx
+++ src/screens/Relation.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Alert } from 'react-native'
 import { TopNavigation } from './components/TopNavigation'
 import { styles } from '../styles'
 import { Relation as RelationModel } from '../model/Relation'
 import { observer } from 'mobx-react'
+import { ConsentoContext } from '../model/ConsentoContext'
+import { User } from '../model/User'
+import { withNavigation, TNavigation } from './navigation'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = (): void => {}
+function confirmAlert (title: string, message: string, onOk: () => void): void {
+  Alert.alert(
+    title,
+    message,
+    [
+      { text: 'Delete', onPress: onOk },
+      { text: 'Abort' }
+    ]
+  )
+}
 
-export const Relation = observer(({ relation }: { relation: RelationModel }): JSX.Element => {
+function confirmDelete (user: User, relation: RelationModel, navigation: TNavigation): void {
+  confirmAlert('Please Confirm', 'Are you sure you want to delete the Relation?', () => {
+    user.relations.delete(relation)
+    navigation.navigate('relations')
+  })
+}
+
+export const Relation = observer(withNavigation(({ relation, navigation }: { relation: RelationModel, navigation: TNavigation }): JSX.Element => {
+  const { user } = useContext(ConsentoContext)
   return <View style={styles.screen}>
-    <TopNavigation title={relation.displayName} back='relations' onDelete={noop} />
+    <TopNavigation title={relation.displayName} back='relations' onDelete={() => confirmDelete(user, relation, navigation)} />
   </View>
-})
+}))

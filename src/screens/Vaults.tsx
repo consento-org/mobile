d86c34ece6f9dfb991f6e4dd579c577c4911dac6
Martin Heidegger <martin.heidegger@gmail.com>
2020-01-11 19:57:06 +0900

    Making sure that the vaults are justified-left

--- src/screens/Vaults.tsx
+++ src/screens/Vaults.tsx
@@ -9,24 +9,41 @@ import { observer } from 'mobx-react'
 import { map } from '../util/map'
 import { Vault } from '../model/Vault'
 import { withNavigation, TNavigation } from './navigation'
+import { useVUnits } from '../util/useVUnits'
+import { elementCardVaultClose } from '../styles/component/elementCardVaultClose'
 
 const listStyle: ViewStyle = {
   display: 'flex',
   flexDirection: 'row',
   flexWrap: 'wrap',
-  justifyContent: 'center',
+  justifyContent: 'flex-start',
   marginTop: 10,
   marginBottom: 20
 }
 
 const AddButton = Asset.buttonAddHexagonal().component
+const HorzPadding = 10
 
 export const VaultsScreen = withNavigation(observer(({ navigation }: { navigation: TNavigation }) => {
   const { user: { vaults } } = useContext(ConsentoContext)
+  const { vw } = useVUnits()
+  const entryWidth = elementCardVaultClose.width
+  let space = vw(100)
+  let count = -1
+  do {
+    if (count === 0) {
+      space -= entryWidth
+    } else {
+      space -= entryWidth + HorzPadding
+    }
+    count++
+  } while (space > 0)
+  const width = count * entryWidth + (count * 2 + 1) * HorzPadding
+
   return <View style={styles.screen}>
     <TopNavigation title='Vaults' />
     <ScrollView centerContent>
-      <View style={listStyle}>
+      <View style={{ ...listStyle, width, left: (vw(100) - width) / 2 }}>
         {
           map(vaults.values(), vault => <VaultCard key={vault.$modelId} vault={vault} />)
         }

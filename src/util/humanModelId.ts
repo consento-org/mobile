bea61a6bcd364cfa1d81eeac2686189fc6508574
Martin Heidegger <martin.heidegger@gmail.com>
2020-01-23 21:59:34 +0900

    Made human id 4 characters shorter while maintaining same data amount

--- src/util/humanModelId.ts
+++ src/util/humanModelId.ts
@@ -2,5 +2,9 @@ import { Buffer } from 'buffer'
 
 export function humanModelId (modelId: string): string {
   const idBuffer = Buffer.from(modelId, 'utf8')
-  return `${idBuffer.readUInt16BE(0).toString(16)}-${idBuffer.readUInt16BE(1).toString(16)}-${idBuffer.readUInt16BE(2).toString(16)}-${idBuffer.readUInt16BE(3).toString(16)}`.toUpperCase()
+  return `${idBuffer.readUInt16BE(0).toString(32)}${idBuffer.readUInt16BE(1).toString(32)}${idBuffer.readUInt16BE(2).toString(32)}${idBuffer.readUInt16BE(3).toString(32)}`
+    .toUpperCase()
+    .split(/(.{4})/g)
+    .filter(Boolean)
+    .join('-')
 }

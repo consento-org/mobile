c5756d146dd59c0eb070bd08f502dfa0e83aa1a6
Martin Heidegger <martin.heidegger@gmail.com>
2020-01-27 04:15:09 +0900

    Code style update

--- src/screens/components/Editor.tsx
+++ src/screens/components/Editor.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { View, ViewStyle, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { elementTextEditor } from '../../styles/component/elementTextEditor'
 import { useVUnits } from '../../styles/Component'
 import { useSafeArea } from 'react-native-safe-area-context'
@@ -13,20 +13,28 @@ import { Color } from '../../styles/Color'
 import { DarkBar } from './DarkBar'
 import { ScreenshotContext } from '../../util/screenshots'
 
-const saveSize: ViewStyle = {
-  position: 'absolute',
-  ...elementTextEditor.saveSize.place.style()
-}
-
-const editSize: ViewStyle = {
-  position: 'absolute',
-  ...elementTextEditor.editSize.place.style()
-}
-
-const closeSize: ViewStyle = {
-  position: 'absolute',
-  ...elementTextEditor.closeSize.place.style()
-}
+const styles = StyleSheet.create({
+  saveSize: {
+    position: 'absolute',
+    ...elementTextEditor.saveSize.place.style()
+  },
+  saveText: {
+    left: elementTextEditor.save.place.left - elementTextEditor.saveSize.place.left,
+    top: elementTextEditor.save.place.top - elementTextEditor.saveSize.place.top
+  },
+  editSize: {
+    position: 'absolute',
+    ...elementTextEditor.editSize.place.style()
+  },
+  editText: {
+    ...elementTextEditor.edit.style,
+    top: elementTextEditor.edit.place.top
+  },
+  closeSize: {
+    position: 'absolute',
+    ...elementTextEditor.closeSize.place.style()
+  }
+})
 
 export interface IEditorProps {
   file: File
@@ -62,18 +70,18 @@ export const Editor = observer(({ navigation, file, vault, children }: IEditorPr
   return <Form>
     <DarkBar />
     <View style={{ position: 'absolute', top: insets.top, height: vh(100) - insets.top, width: '100%' }}>
-      <TouchableOpacity style={closeSize} onPress={() => leave(() => navigation.navigate('vault', { vault: vault.$modelId }))}>
+      <TouchableOpacity style={styles.closeSize} onPress={() => leave(() => navigation.navigate('vault', { vault: vault.$modelId }))}>
         <elementTextEditor.close.Render />
       </TouchableOpacity>
       {
         isDirty
-          ? <TouchableOpacity style={saveSize} onPress={save}>
-            {elementTextEditor.save.render({ style: { left: elementTextEditor.save.place.left - elementTextEditor.saveSize.place.left, top: elementTextEditor.save.place.top - elementTextEditor.saveSize.place.top } })}
+          ? <TouchableOpacity style={styles.saveSize} onPress={save}>
+            {elementTextEditor.save.render({ style: styles.saveText })}
           </TouchableOpacity>
           : <></>
       }
-      <TouchableOpacity style={{ ...editSize, left: vw(100) - (elementTextEditor.width - elementTextEditor.edit.place.left) }} onPress={event => open([], null, event)}>
-        <Text style={{ ...elementTextEditor.edit.style, top: elementTextEditor.edit.place.top }}>{elementTextEditor.edit.text}</Text>
+      <TouchableOpacity style={{ ...styles.editSize, left: vw(100) - (elementTextEditor.width - elementTextEditor.edit.place.left) }} onPress={event => open([], null, event)}>
+        <Text style={styles.editText}>{elementTextEditor.edit.text}</Text>
       </TouchableOpacity>
       <elementTextEditor.title.Render
         onInstantEdit={filename.handleValue}

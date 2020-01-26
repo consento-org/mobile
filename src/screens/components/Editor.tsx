435ec2818a48eb7f33ba4d5b780280a1dec91f33
Martin Heidegger <martin.heidegger@gmail.com>
2020-01-27 07:27:43 +0900

    Better layout in the editors

--- src/screens/components/Editor.tsx
+++ src/screens/components/Editor.tsx
@@ -1,9 +1,7 @@
 import React, { useContext } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
 import { elementTextEditor } from '../../styles/component/elementTextEditor'
 import { useVUnits } from '../../styles/Component'
-import { useSafeArea } from 'react-native-safe-area-context'
-import { ContextMenuContext } from './ContextMenu'
 import { File, FileType } from '../../model/VaultData'
 import { useForm } from '../../util/useForm'
 import { observer } from 'mobx-react'
@@ -19,16 +17,10 @@ const styles = StyleSheet.create({
     ...elementTextEditor.saveSize.place.style()
   },
   saveText: {
-    left: elementTextEditor.save.place.left - elementTextEditor.saveSize.place.left,
-    top: elementTextEditor.save.place.top - elementTextEditor.saveSize.place.top
-  },
-  editSize: {
-    position: 'absolute',
-    ...elementTextEditor.editSize.place.style()
-  },
-  editText: {
-    ...elementTextEditor.edit.style,
-    top: elementTextEditor.edit.place.top
+    ...elementTextEditor.save.style,
+    top: elementTextEditor.save.place.top,
+    left: 0,
+    width: elementTextEditor.save.place.width
   },
   closeSize: {
     position: 'absolute',
@@ -46,7 +38,6 @@ export interface IEditorProps {
 
 export const Editor = observer(({ navigation, file, vault, children }: IEditorProps): JSX.Element => {
   const { vh, vw } = useVUnits()
-  const { open } = useContext(ContextMenuContext)
   const screenshots = useContext(ScreenshotContext)
   const { Form, useField, save, leave, isDirty } = useForm(navigation, undefined, (): any => navigation.navigate('vault', { vault: vault.$modelId }))
   const filename = useField(
@@ -55,8 +46,6 @@ export const Editor = observer(({ navigation, file, vault, children }: IEditorPr
     (newName: string): boolean => newName === file.name || vault.data.isUnusedFilename(newName),
     (newName: string): void => vault.data.setFilename(file, newName)
   )
-  const insets = useSafeArea()
-
   if (!isDirty) {
     if (!/^Untitled-/.test(file.name)) {
       if (file.type === FileType.image) {
@@ -68,33 +57,29 @@ export const Editor = observer(({ navigation, file, vault, children }: IEditorPr
   }
 
   return <Form>
-    <DarkBar />
-    <View style={{ position: 'absolute', top: insets.top, height: vh(100) - insets.top, width: '100%' }}>
-      <TouchableOpacity style={styles.closeSize} onPress={() => leave(() => navigation.navigate('vault', { vault: vault.$modelId }))}>
-        <elementTextEditor.close.Render />
-      </TouchableOpacity>
-      {
-        isDirty
-          ? <TouchableOpacity style={styles.saveSize} onPress={save}>
-            {elementTextEditor.save.render({ style: styles.saveText })}
-          </TouchableOpacity>
-          : <></>
-      }
-      <TouchableOpacity style={{ ...styles.editSize, left: vw(100) - (elementTextEditor.width - elementTextEditor.edit.place.left) }} onPress={event => open([], null, event)}>
-        <Text style={styles.editText}>{elementTextEditor.edit.text}</Text>
-      </TouchableOpacity>
-      <elementTextEditor.title.Render
-        onInstantEdit={filename.handleValue}
-        style={filename.isInvalid ? { color: Color.red } : undefined}
-        value={filename.initial}
-      />
-      <elementTextEditor.size.Render />
+    <View style={{ position: 'absolute', height: vh(100), width: '100%', top: 0, left: 0, display: 'flex', justifyContent: 'flex-start', alignContent: 'stretch' }}>
+      <DarkBar />
+      <View style={{ height: elementTextEditor.size.place.bottom }}>
+        <TouchableOpacity style={styles.closeSize} onPress={() => leave(() => navigation.navigate('vault', { vault: vault.$modelId }))}>
+          <elementTextEditor.close.Render />
+        </TouchableOpacity>
+        {
+          isDirty
+            ? <TouchableOpacity style={{ ...styles.saveSize, left: vw(100) - (elementTextEditor.width - elementTextEditor.save.place.left) }} onPress={save}>
+              {elementTextEditor.save.render({ style: styles.saveText })}
+            </TouchableOpacity>
+            : <></>
+        }
+        <elementTextEditor.title.Render
+          onInstantEdit={filename.handleValue}
+          style={filename.isInvalid ? { color: Color.red } : undefined}
+          value={filename.initial}
+        />
+        <elementTextEditor.size.Render />
+      </View>
       <View
         style={{
-          position: 'absolute',
-          top: elementTextEditor.readable.place.top,
-          height: vh(100) - insets.top - elementTextEditor.readable.place.top - insets.bottom,
-          width: '100%'
+          flexGrow: 1
         }}
       >{children}</View>
     </View>

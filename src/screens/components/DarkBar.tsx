83647f7855cd49a7ec8a40d03ff979e2031d053b
Martin Heidegger <martin.heidegger@gmail.com>
2020-01-27 07:25:51 +0900

    Made sure that the status bar is visible on ios

--- src/screens/components/DarkBar.tsx
+++ src/screens/components/DarkBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useSafeArea } from 'react-native-safe-area-context'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import { Color } from '../../styles/Color'
 
 export const DarkBar = (): JSX.Element => {
   const insets = useSafeArea()
-  return <View style={{ width: '100%', height: insets.top, backgroundColor: Color.veryDarkGrey }} />
+  return <View style={{ width: '100%', height: insets.top, backgroundColor: Platform.OS === 'ios' ? Color.lightGrey : Color.veryDarkGrey }} />
 }

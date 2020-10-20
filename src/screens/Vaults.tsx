d7f2973074a79a5021e91aab50d1a93ec53f41ea
Martin Heidegger <martin.heidegger@gmail.com>
2020-10-21 02:32:39 +0900

    fix: re-adding background color of previous version
    Signed-off-by: Martin Heidegger <martin.heidegger@gmail.com>

--- src/screens/Vaults.tsx
+++ src/screens/Vaults.tsx
@@ -13,10 +13,12 @@ import { useAutorun } from '../util/useAutorun'
 import { comparer } from 'mobx'
 import { elementVaultsEmpty } from '../styles/design/layer/elementVaultsEmpty'
 import { assertExists } from '../util/assertExists'
+import { Color } from '../styles/design/Color'
 
 const AddButton = ImageAsset.buttonAddHexagonal
 const styles = StyleSheet.create({
   container: { flexGrow: 1 },
+  vaultGrid: { backgroundColor: Color.lightGrey },
   add: { position: 'absolute', right: 10, bottom: 10, zIndex: 1 }
 })
 
@@ -56,7 +58,7 @@ export const VaultsScreen = observer((): JSX.Element => {
     <TopNavigation title='Vaults' />
     <SketchImage src={AddButton} style={styles.add} onPress={handlePress} />
     <EmptyView isEmpty={useAutorun(() => vaults.size === 0)}>
-      <MobxGrid ref={ref} data={vaults} itemStyle={VAULT_STYLE} renderItem={renderVault} />
+      <MobxGrid ref={ref} data={vaults} itemStyle={VAULT_STYLE} renderItem={renderVault} style={styles.vaultGrid} />
     </EmptyView>
   </View>
 })
